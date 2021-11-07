429e1347ded08aad4bed58567a8547357d0c5b24
Lusi0 <ItsLusio@gmail.com>
2021-11-06 22:03:28 -0700

    Update convert.py

--- convert.py
+++ convert.py
@@ -15,14 +15,37 @@ for line in h:
         i = mydict[y]
         mydict.update({y:(i[0],i[1]+1)})
 
+newdict = {}
+highest = None
+lowest = None
+for key in mydict:
+    if lowest == None:
+        lowest = int(key)
+    if highest == None:
+        highest = int(key)
+    if int(key) > highest:
+        highest = int(key)
+    if int(key) < lowest:
+        lowest = int(key)
+
+for i in range(lowest,highest+1,20):
+    newdict.update({i:(0,0)})
+
+def myround(x, base=5):
+    return base * round(x/base)
 
 for key in mydict:
-    i = mydict[key]
-    mydict.update({key:(i[0]/i[1])})
-print(mydict)
+    i = newdict[str(myround(float(key),20))]
+    newdict.update({myround(float(key),20):(i[0]+mydict[key][0],i[1]+mydict[key][1])})
+print(newdict)
+    
+# for key in mydict:
+#     i = mydict[key]
+#     mydict.update({key:(i[0]/i[1])})
+# print(mydict)
 
-f = open("new.txt","w")
-for key in mydict:
-    f.write("{},{}\n".format(key,mydict[key]))
+# f = open("new.txt","w")
+# for key in mydict:
+#     f.write("{},{}\n".format(key,mydict[key]))
     
     
